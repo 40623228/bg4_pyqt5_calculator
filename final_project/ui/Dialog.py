f384ad38d6eae0e35f83e71de58bdccbfc6cc5dc
40623220 <40623220@gm.nfu.edu.tw>
2017-12-18 15:02:51 +0800

    report and assign

--- final_project/ui/Dialog.py
+++ final_project/ui/Dialog.py
@@ -26,6 +26,7 @@ class Dialog(QDialog, Ui_Dialog):
         '''以下為使用者自行編寫程式碼區'''
 
     def digitClicked(self):
+#40623228
         '''
         使用者按下數字鍵, 必須能夠累積顯示該數字
         當顯示幕已經為 0, 再按零不會顯示 00, 而仍顯示 0 或 0.0
@@ -34,54 +35,67 @@ class Dialog(QDialog, Ui_Dialog):
         pass
         
     def unaryOperatorClicked(self):
+#40623221
         '''單一運算元按下後處理方法'''
         pass
         
     def additiveOperatorClicked(self):
+#40623229
         '''加或減按下後進行的處理方法'''
         pass
         
     def multiplicativeOperatorClicked(self):
+#40623220
         '''乘或除按下後進行的處理方法'''
         pass
         
     def equalClicked(self):
+#40623228
         '''等號按下後的處理方法'''
         pass
         
     def pointClicked(self):
+#40623221
         '''小數點按下後的處理方法'''
         pass
         
     def changeSignClicked(self):
+#40623220        
         '''變號鍵按下後的處理方法'''
         pass
         
     def backspaceClicked(self):
+#40623229
         '''回復鍵按下的處理方法'''
         pass
         
     def clear(self):
+#40623229
         '''清除鍵按下後的處理方法'''
         pass
         
     def clearAll(self):
+#40623221
         '''全部清除鍵按下後的處理方法'''
         pass
         
     def clearMemory(self):
+#40623230
         '''清除記憶體鍵按下後的處理方法'''
         pass
         
     def readMemory(self):
+#40623230
         '''讀取記憶體鍵按下後的處理方法'''
         pass
         
     def setMemory(self):
+#40623230
         '''設定記憶體鍵按下後的處理方法'''
         pass
         
     def addToMemory(self):
+#40623230
         '''放到記憶體鍵按下後的處理方法'''
         pass
         
@@ -90,9 +104,11 @@ class Dialog(QDialog, Ui_Dialog):
         pass
         
     def abortOperation(self):
+#40623220        
         '''中斷運算'''
         pass
         
     def calculate(self):
+#40623220
         '''計算'''
         pass
